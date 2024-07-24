9cac258879e30b67e4e61a907f0ba7f567632175
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-24 13:27:52 -0400

    Complete all aggregate methods

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,14 @@
 class Actor < ActiveRecord::Base
   has_many :auditions
   has_many :roles, through: :auditions
+
+  def self.most_roles
+    most_roles = Audition.group(:actor_id).count.max_by { |_, count| count }.first
+    find(most_roles)
+  end
+
+  def self.hired_roles
+    hired_actors = Audition.where(hired: true).pluck(:actor_id)
+    find(hired_actors)
+  end
 end
